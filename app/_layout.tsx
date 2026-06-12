c69135794d0bb8e48e7a57389b646d5137393e6f
dowofox <dhqxlajtm103@gmail.com>
2026-06-13 03:20:15 +0900

    Register clothes detail route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout() {
         <Stack.Screen name="history" options={{ headerShown: false }} />
         <Stack.Screen name="profile" options={{ headerShown: false }} />
         <Stack.Screen name="closet" options={{ headerShown: false }} />
+        <Stack.Screen name="clothes-detail" options={{ headerShown: false }} />
         <Stack.Screen name="add-clothes" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
       </Stack>
